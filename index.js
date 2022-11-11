aeccb00d298867063bffa3c59adfd49ce042fcb0
Jakaria230161 <jakariamahmud016@gmail.com>
2022-11-11 20:36:10 +0600

    projecst done

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-
+// verify jwt
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
